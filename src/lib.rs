ed7bd98ba1b2c4827989d9bd4cdf1e0672bbb48d
Alvin Pan <30609374+alfaevc@users.noreply.github.com>
2020-06-02 23:19:55 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 use wasm_bindgen::prelude::*;
-use serde::{Serialize, Deserialize};
+//use serde::{Serialize, Deserialize};
 use serde_json;
-use tera::{Context, Tera, Value};
+use tera::{Context, Tera};
+// use std::clone;
+// use std::os::raw::{c_char};
+// use std::ffi::{CString};
 
 #[derive(Clone, Debug)]
 pub struct Graph {
@@ -25,26 +28,30 @@ impl Graph {
       }
   }
 
-
   pub fn add_point(&mut self, x: f64, y: f64) {
       self.points.push(Point { x, y });
   }
 
-  pub fn draw_svg(&self, width: usize, height: usize) -> String {
+  pub fn draw_svg(&self, width: usize, height: usize, padding: usize, path: String, file: &str) -> String {
 
     let mut context = Context::new();
+
   
     //hardset the padding around the graph
-    let padding = 50;
-  
+
+    // let c_str = CString::new(file).unwrap();
+    // let filename: *const c_char = c_str.as_ptr() as *const c_char;
+    // const filename: &str = file.clone();
+
     //ensure the viewbox is as per input
-    let width = width - padding * 2;
-    let height = height - padding * 2;
   
     context.insert("name", &self.name);
     context.insert("width", &width);
     context.insert("height", &height);
     context.insert("padding", &padding);
+    context.insert("path", &path);
+    context.insert("colour", &self.colour);
+    context.insert("lines", &5);
   
     Tera::one_off(include_str!("graph.svg"), &context, true).expect("Could not draw graph")
     
@@ -53,7 +60,7 @@ impl Graph {
 
 
 pub fn generate_graph(xs: Vec<f64>, ys: Vec<f64>) -> Graph {
-  let mut graph = Graph::new("Example".into(), "#8ff0a4".into());
+  let mut graph = Graph::new("SVG".into(), "#8ff0a4".into());
   for i in 0..xs.len() {
     graph.add_point(xs[i], ys[i]);
   }
@@ -68,9 +75,44 @@ pub fn generate_graph(xs: Vec<f64>, ys: Vec<f64>) -> Graph {
 }*/
 
 #[wasm_bindgen]
-pub fn get_svg(xstr: &str, ystr: &str) {
+pub fn get_svg(xstr: &str, ystr: &str, width: usize, height: usize, padding: usize, file: &str) -> String {
   let xs: Vec<f64> = serde_json::from_str(&xstr).unwrap();
   let ys: Vec<f64> = serde_json::from_str(&ystr).unwrap();
   let graph = generate_graph(xs, ys);
-  println!("{}", graph.draw_svg(800, 400));
+  let width = width - padding * 2;
+  let height = height - padding * 2;
+  //let min_x = graph.points.get(0).map(|val| val.x).unwrap_or(0.0);
+  let max_x = graph
+    .points
+    .iter()
+    .map(|point| point.x)
+    .fold(0. / 0., f64::max);
+  
+  //let min_y = graph.points.iter().map(|val| val.y).fold(0. / 0., f64::min);
+  let max_y = graph
+    .points
+    .iter()
+    .map(|point| point.y)
+    .fold(0. / 0., f64::max);
+
+  let path = graph
+              .points
+              .iter()
+              .map(|val| Point {
+                  x: (val.x / max_x * width as f64) + padding as f64,
+                  y: (val.y / max_y * (height as f64 * -1.0)) + (padding + height) as f64,
+              })
+              .enumerate()
+              .map(|(i, point)| {
+                  if i == 0 {
+                      format!("M {} {}", point.x, point.y)
+                  } else {
+                      format!("L {} {}", point.x, point.y)
+                  }
+              })
+              .collect::<Vec<String>>().join(" ");
+  
+  let out = graph.draw_svg(width, height, padding, path, file);
+  println!("{}", out);
+  return out;
 }
