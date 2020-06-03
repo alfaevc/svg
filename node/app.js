a953e91af788b28b3ee77a7c487167cc01d17212
Alvin Pan <30609374+alfaevc@users.noreply.github.com>
2020-06-02 23:18:46 -0400

    Update app.js

--- node/app.js
+++ node/app.js
@@ -1,7 +1,10 @@
 const { get_svg } = require('../pkg/svg_lib.js');
 
-var x = [10., 5.];
-var y = [2., 8.];
+var x = [1., 2., 3., 4., 5.];
+var y = [1., 3., 2.5, 6., 3.];
+var width = 800;
+var height = 400;
+var p = 50;
 
-console.log(get_svg(JSON.stringify(x), JSON.stringify(y)));
+console.log(get_svg(JSON.stringify(x), JSON.stringify(y), width, height, p, "graph.svg"));
 // resize_file(JSON.stringify([dim, 'Vincent_van_Gogh_368.jpg', `test.png`]));
