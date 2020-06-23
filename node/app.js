e22353dd40e6f36137e469114010b4340c6bebc0
Alvin Pan <30609374+alfaevc@users.noreply.github.com>
2020-06-23 15:53:30 -0400

    Update app.js

--- node/app.js
+++ node/app.js
@@ -2,14 +2,15 @@ const { get_svg } = require('../pkg/svg_lib.js');
 const fs = require('fs');
 
 var http = require('http')
-var x = [1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12.];
-var y = [10., 3., 17., 20., 25., 23., 30., 38.2, 32.5, 40.8, 35.7, 21.7];
+// var x = [1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12.];
+// var y = [10., 3., 17., 20., 25., 23., 30., 38.2, 32.5, 40.8, 35.7, 21.7];
 var width = 800;
 var height = 400;
 var p = 50;
 var title = "SVG";
 
-var out = get_svg(JSON.stringify(x), JSON.stringify(y), width, height, p, title)
+var iris_csv = fs.readFileSync("node/iris.data.csv");
+var out = get_svg(iris_csv, width, height, p, title)
 
 fs.writeFile('src/out.svg', out, (err) => {
     if (err) throw err;
