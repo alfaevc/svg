e6a95ed1935b60a4a4b413cbc725e27ef1313e78
Ubuntu <ubuntu@Intern-CMU.4t0qlmzzuahelg1ke4z0fuavag.bx.internal.cloudapp.net>
2020-06-25 04:12:48 +0000

    scatterplot

--- src/lib.rs
+++ src/lib.rs
@@ -45,10 +45,15 @@ impl Graph {
       self.points.push(Point { x, y });
   }
 
-  pub fn draw_svg(&self, width: usize, height: usize, padding: usize, path: String) -> String {
+  pub fn draw_svg(&self, width: usize, height: usize, padding: usize, path: Vec<Point>) -> String {
 
     let mut context = Context::new();
+    
+    let mut p: Vec<(f64, f64)> = Vec::new();
 
+    for point in path {
+      p.push((point.x, point.y));
+    }
     //let min_x = graph.points.get(0).map(|val| val.x).unwrap_or(0.0);
     /*let max_x = self
       .points
@@ -74,7 +79,7 @@ impl Graph {
     context.insert("width", &width);
     context.insert("height", &height);
     context.insert("padding", &padding);
-    context.insert("path", &path);
+    context.insert("path", &p);
     context.insert("max_x", &self.max_x);
     context.insert("max_y", &self.max_y);
     context.insert("colour", &self.colour);
@@ -163,16 +168,16 @@ pub fn get_svg(csv_content: &[u8], width: usize, height: usize, padding: usize, 
                   //y: (val.y / graph.max_y * (height as f64 * -1.0)) + (padding + height) as f64,
                   x: ((val.x+graph.max_x) / (2.0*graph.max_x) * width as f64) + padding as f64,
                   y: ((val.y+graph.max_y) / (2.0*graph.max_y) * (height as f64 * -1.0)) + (padding + height) as f64,
-              })
-              .enumerate()
-              .map(|(i, point)| {
-                  if i == 0 {
-                      format!("M {} {}", point.x, point.y)
-                  } else {
-                      format!("L {} {}", point.x, point.y)
-                  }
-              })
-              .collect::<Vec<String>>().join(" ");
+              }).collect();
+            //  .enumerate()
+            //  .map(|(i, point)| {
+            //      if i == 0 {
+            //          format!("M {} {}", point.x, point.y)
+            //      } else {
+            //          format!("L {} {}", point.x, point.y)
+            //      }
+            //  })
+            //  .collect::<Vec<String>>().join(" ");
 
   let out = graph.draw_svg(width, height, padding, path);
   // println!("{}", out);
